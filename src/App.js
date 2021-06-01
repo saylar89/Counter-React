9be4f5581ccb9f9eb3089129d06d713dd2675f4b
Aslan Ashtiani <saylar.design@gmail.com>
2021-06-01 15:01:41 +0430

    Counter project - the End

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [color, setColor] = useState("black");
+  const [text, setText] = useState("Grey");
+  const [content, setContent] = useState("");
+  const [author, setAuthor] = useState("");
+
+  const changeToGrey = () => {
+    setColor("grey");
+    setText("B  lack");
+  };
+
+  const changeToBlack = () => {
+    setColor("black");
+    setText("Grey");
+  };
+
+  useEffect(() => {
+    fetch("https://api.quotable.io/random")
+      .then((response) => response.json())
+      .then((data) => {
+        setContent(data.content);
+        setAuthor(data.author);
+      });
+  }, [count]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className={`container ${color}`}>
+        <h1>Counter with Hooks</h1>
+        <p>You clicked {count} times</p>
+        <div className="btns">
+          <button onClick={() => setCount(count + 1)} className="btn">
+            Click Me
+          </button>
+          <button
+            onClick={() => {
+              color === "black" ? changeToGrey() : changeToBlack();
+            }}
+            className="btn"
+          >
+            {`Change to ${text}`}
+          </button>
+          <button onClick={() => setCount(0)} className="btn">
+            Reset
+          </button>
+        </div>
+      </div>
+      <div className="quote">
+        <h1>{content}</h1>
+        <span>{author}</span>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
